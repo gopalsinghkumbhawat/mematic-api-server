a551f481adeae92524fee234d50de28754c32709
harshsharma22 <harshsharma199622@gmail.com>
2020-04-06 14:43:11 +0530

    added meme model

---
+++ src/models/memer.model.ts
@@ -0,0 +1,54 @@
+import {Entity, model, property} from '@loopback/repository';
+
+export class Memer extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: false,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  creationTime: Date;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  publishedTime: Date;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  memeStatus: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  memeUrl: string;
+  @property({
+    type: 'string',
+    required: true,
+  })
+  likes: string;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
